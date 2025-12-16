49046ca8acfd36cb1f056a8bd0b61c66237c684a
JJ <jaju42190@edu.ucl.dk>
2025-12-16 14:23:25 +0100

    Add project files.

---
+++ CatalogService.Domain/Entities/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalogService.Domain.Entities;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public string ISBN { get; set; } = string.Empty;
+    public bool IsAvailable { get; set; } = true;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
+}
